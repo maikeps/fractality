7ac52e63566da2fa443957861edbf6860277a768
Ranieri Althoff <ranisalt@gmail.com>
2015-06-11 01:37:31 -0300

    Fix indentation

--- fractality.py
+++ fractality.py
@@ -37,105 +37,114 @@ def create_quadrants(canvas_startx, canvas_starty, canvas_width, canvas_height,
 
 
 def crazy(hex_color, rect_number):
-	if hex_color[0] == '#':
-		hex_color = '0x'+hex_color[1::]
-	number = int(hex_color, 16)
-	final_hex = hex(number*int(rect_number))[2::]
-	if len(final_hex) > 6:
-		final_hex = final_hex[len(final_hex)-6::]
-	if len(final_hex) < 6:
-		aux = ''
-		for i in range(len(final_hex) - 6):
-			aux += '0'
-		final_hex = aux + final_hex
+    if hex_color[0] == '#':
+        hex_color = '0x' + hex_color[1::]
+    number = int(hex_color, 16)
+    final_hex = hex(number * int(rect_number))[2::]
+    if len(final_hex) > 6:
+        final_hex = final_hex[len(final_hex) - 6::]
+    if len(final_hex) < 6:
+        aux = ''
+        for i in range(len(final_hex) - 6):
+            aux += '0'
+        final_hex = aux + final_hex
 
-	return "#"+final_hex
+    return "#" + final_hex
 
 
+def generate_fractal(regex, main_color, secondary_color, depth,
+                     coloring_function=None):
+    master = tkinter.Tk()
 
-def generate_fractal(regex, main_color, secondary_color, depth, coloring_function=None):
-	master = tkinter.Tk()
+    canvas = tkinter.Canvas(width=600, height=600)
+    canvas.pack()
 
-	canvas = tkinter.Canvas(width=600, height=600)
-	canvas.pack()
+    rects = create_quadrants(0, 0, 600, 600, depth)
 
-	rects = create_quadrants(0, 0, 600, 600, depth)
+    for rect in rects:
+        color = ''
+        if re.search(regex, rect['name']):
+            if coloring_function:
+                color = globals()[coloring_function](main_color, rect['name'])
+            else:
+                color = main_color
+        else:
+            color = secondary_color
+        canvas.create_rectangle(rect['x'], rect['y'], rect['x2'], rect['y2'],
+                                fill=color, outline=color)
 
-	for rect in rects:
-		color = ''
-		if re.search(regex, rect['name']):
-			if coloring_function:
-				color = globals()[coloring_function](main_color, rect['name'])
-			else:
-				color = main_color
-		else:
-			color = secondary_color
-		canvas.create_rectangle(rect['x'], rect['y'], rect['x2'], rect['y2'], fill=color, outline=color)
+    master.mainloop()
 
-	#master.mainloop()
 
 def main():
-	if len(sys.argv) > 1:
-		if sys.argv[1] == '--help' or sys.argv[1] == '-h':
-			print_help()
-		else:
-			if not arguments_correct(sys.argv):
-				print("Arguments incorrect")
-				return 0
-			regex = find_arg('-r', sys.argv)
-			if not regex_correct(regex):
-				print("Regular Expression malformed")
-				return 0
-			main_color = find_arg('-p', sys.argv).upper()
-			secondary_color = find_arg('-s', sys.argv).upper()
-			coloring_function = ''
-			if find_arg('-f', sys.argv) is not None:
-				coloring_function = find_arg('-f', sys.argv).lower()
-			depth = 8
-			if find_arg('-d', sys.argv) is not None:
-				depth = int(find_arg('-d', sys.argv))
-			print(coloring_function)
-			generate_fractal(regex, main_color, secondary_color, depth, coloring_function)
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '--help' or sys.argv[1] == '-h':
+            print_help()
+        else:
+            if not arguments_correct(sys.argv):
+                print("Arguments incorrect")
+                return 0
+            regex = find_arg('-r', sys.argv)
+            if not regex_correct(regex):
+                print("Regular Expression malformed")
+                return 0
+            main_color = find_arg('-p', sys.argv).upper()
+            secondary_color = find_arg('-s', sys.argv).upper()
+            coloring_function = ''
+            if find_arg('-f', sys.argv) is not None:
+                coloring_function = find_arg('-f', sys.argv).lower()
+            depth = 8
+            if find_arg('-d', sys.argv) is not None:
+                depth = int(find_arg('-d', sys.argv))
+            print(coloring_function)
+            generate_fractal(regex, main_color, secondary_color, depth,
+                             coloring_function)
+
 
 def find_arg(arg, argv):
-	if arg in argv:
-		return argv[argv.index(arg)+1]
+    if arg in argv:
+        return argv[argv.index(arg) + 1]
+
 
 def regex_correct(regex):
-	return True
+    return True
+
 
 def arguments_correct(argv):
-	if len(argv) < 6:
-		return False
-	return True
+    if len(argv) < 6:
+        return False
+    return True
+
 
 def print_help():
-	BOLD = '\033[1m'
-	END = '\033[0m'
+    BOLD = '\033[1m'
+    END = '\033[0m'
 
-	print("\n")
-	print(BOLD+"Fractality - Regex Fractal Generator:"+END)
-	print("Fractality is a fractal generator based on regular expression.\n")
+    print("\n")
+    print(BOLD + "Fractality - Regex Fractal Generator:" + END)
+    print("Fractality is a fractal generator based on regular expression.\n")
 
-	print(BOLD+"Usage:"+END)
-	print("-r <arg>\tThe regular expression used to generate the fractal")
-	print("-f <arg>\tDefines the fractal coloring function (optional)")
-	print("-p <arg>\tThe primary color used on the coloring function")
-	print("-s <arg>\tThe secondary color used on the coloring function")
-	print("-d <arg>\tThe detail depth of the fractal (optional)\n")
-	print("OBS:\nThe arguments don't need to be in order;\nAll non-integer arguments need to be in quotes;\nNote that the depth must be a number between 1 and 9, as it increases exponencially the amount of memory needed to generate the fractal.\n")
+    print(BOLD + "Usage:" + END)
+    print("-r <arg>\tThe regular expression used to generate the fractal")
+    print("-f <arg>\tDefines the fractal coloring function (optional)")
+    print("-p <arg>\tThe primary color used on the coloring function")
+    print("-s <arg>\tThe secondary color used on the coloring function")
+    print("-d <arg>\tThe detail depth of the fractal (optional)\n")
+    print(
+        "OBS:\nThe arguments don't need to be in order;\nAll non-integer arguments need to be in quotes;\nNote that the depth must be a number between 1 and 9, as it increases exponencially the amount of memory needed to generate the fractal.\n")
 
-	print(BOLD+"Coloring functions:"+END)
-	print("crazy\n")
+    print(BOLD + "Coloring functions:" + END)
+    print("crazy\n")
 
-	print(BOLD+"Regular Expresison examples:"+END)
-	print("(23|41|34|12)")
-	print("(13|31|24|42)")
-	print("(13|31)")
-	print("(1)\n\n")
+    print(BOLD + "Regular Expresison examples:" + END)
+    print("(23|41|34|12)")
+    print("(13|31|24|42)")
+    print("(13|31)")
+    print("(1)\n\n")
 
-	print("Made by Maike de Paula Santos")
-	print("Inspired by ssodelta: http://ssodelta.wordpress.com")
+    print("Made by Maike de Paula Santos")
+    print("Inspired by ssodelta: http://ssodelta.wordpress.com")
+
 
 if __name__ == '__main__':
-	main()
+    main()
